c44d520932f2f41108ed3200fda53750aa6f39f9
brayanalvarez98 <baalvarezc@ufpso.edu.co>
2024-06-25 18:16:50 -0500

    prueba primer commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,21 +1,5 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
 test('test', async ({ page }) => {
   await page.goto('https://app.universoacdc.com/');
   await page.getByPlaceholder('Correo electrónico').click();
@@ -26,13 +10,9 @@ test('test', async ({ page }) => {
   // Haz clic en el botón de inicio de sesión
   await page.getByRole('button', { name: 'Iniciar sesión' }).click();
   await page.getByRole('banner').locator('span').click();
-  await page.getByRole('button', { name: 'Misiones Diarias' }).click();
+  await page.getByRole('button', { name: 'Mi perfil' }).click();
 
   // Espera un momento para verificar que la página se ha cargado después del inicio de sesión
   await page.waitForTimeout(5000); 
 
-    
-
-
-
-  });
+      });
